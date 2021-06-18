0f8fccbdf6632f0df540cebe55b1e20776f2c253
donguk.seo <donguk.seo@modori.io>
2021-06-18 16:54:43 +0900

    Impl dToken mapping

---
+++ src/mappings/DTokenMapping.ts
@@ -0,0 +1,33 @@
+import {
+  Mint as MintEvent,
+  Burn as BurnEvent,
+} from '../../generated/DToken/DToken';
+import {
+  findOrCreateDTokenUserBalance,
+} from './utils/initializers';
+
+export function handleMint(event: MintEvent): void {
+  let dTokenUserBalance = findOrCreateDTokenUserBalance(
+    event.params.account.toHex(),
+    event.address.toHex(),
+    event.block.timestamp
+  );
+
+  dTokenUserBalance.balance = dTokenUserBalance.balance.plus(event.params.amount);
+  dTokenUserBalance.lastUpdatedTimestamp = event.block.timestamp.toI32();
+
+  dTokenUserBalance.save();
+}
+
+export function handleBurn(event: BurnEvent): void {
+  let dTokenUserBalance = findOrCreateDTokenUserBalance(
+    event.params.account.toHex(),
+    event.address.toHex(),
+    event.block.timestamp
+  );
+
+  dTokenUserBalance.balance = dTokenUserBalance.balance.minus(event.params.amount);
+  dTokenUserBalance.lastUpdatedTimestamp = event.block.timestamp.toI32();
+
+  dTokenUserBalance.save();
+}
--- src/mappings/utils/initializers.ts
+++ src/mappings/utils/initializers.ts
@@ -1,5 +1,25 @@
-import { User } from "../../../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { User, DTokenUserBalance } from "../../../generated/schema"
 
 export function findOrCreateUser(id: string): User {
   return (User.load(id) || new User(id)) as User
+}
+
+export function findOrCreateDTokenUserBalance(
+  user: string,
+  dToken: string,
+  timestamp: BigInt
+): DTokenUserBalance {
+  let dTokenUserBalance = DTokenUserBalance.load(user + dToken);
+
+  if (!dTokenUserBalance) {
+    dTokenUserBalance = new DTokenUserBalance(user + dToken);
+    dTokenUserBalance.balance = BigInt.fromString('0');
+    dTokenUserBalance.user = user;
+    dTokenUserBalance.dToken = dToken;
+    dTokenUserBalance.lastUpdatedTimestamp = timestamp.toI32();
+    dTokenUserBalance.save();
+  }
+
+  return dTokenUserBalance as DTokenUserBalance
 }
